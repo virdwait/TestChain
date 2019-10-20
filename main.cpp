97a2167163cfb895cbc6034335d9299e87863188
V Developers <virdwait@gmail.com>
2019-10-20 13:34:09 +0530

    added comments

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 
 int main()
 {
-    Blockchain bChain = Blockchain();
+    Blockchain bChain = Blockchain(); // initialize Class object
 
     cout << "Mining block 1..." << endl;
-    bChain.AddBlock(Block(1, "Block 1 Data"));
+    bChain.AddBlock(Block(1, "Block 1 Data")); // Adding block
 
     cout << "Mining block 2..." << endl;
-    bChain.AddBlock(Block(2, "Block 2 Data"));
+    bChain.AddBlock(Block(2, "Block 2 Data")); // Adding block
 
     cout << "Mining block 3..." << endl;
-    bChain.AddBlock(Block(3, "Block 3 Data"));
+    bChain.AddBlock(Block(3, "Block 3 Data")); // Adding block
 
     return 0;
 }
